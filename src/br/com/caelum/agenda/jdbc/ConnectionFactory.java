c081b7b75e90daf64abef0d08fc71d3f8e8777a9
Leonardo Soares <leoss33@outlook.com.br>
2019-07-07 00:45:49 -0300

    First commit

---
+++ src/br/com/caelum/agenda/jdbc/ConnectionFactory.java
@@ -0,0 +1,21 @@
+package br.com.caelum.agenda.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    public Connection getConnection() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            return DriverManager.getConnection(
+                    "jdbc:postgresql://localhost/Caelum", "leonardo", "6322Leo0");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Erro na conexão do bd");
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+}
